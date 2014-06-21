2ae1ed634057c2dda1b290f3a83deb53ef484d47
Lance Nanek <lnanek@gmail.com>
2014-06-21 01:43:41 -0700

    Require two movements to trigger count

--- android-client/src/us/gpop/classpulse/MainActivity.java
+++ android-client/src/us/gpop/classpulse/MainActivity.java
@@ -29,65 +29,71 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends RoboActivity {
-	
+
 	private static class GlassSetup {
 		private static Detector setup(final DetectorListener listener, Context context) {
 			return new SwipeDetector(listener, context);
 		}
 	}
-	
+
+	private static enum HeadMotion {
+		LEFT, RIGHT, UP, DOWN;
+	}
+
 	private static final int POLL_PERIOD_MS = 5 * 1000;
-	
+
 	private static final float NOD_TRIGGER_SUM = 25;
 
 	private static final float SHAKE_TRIGGER_SUM = 25;
 
 	private static final String LOG_TAG = MainActivity.class.getSimpleName();
-	
+
 	private Handler handler;
 
 	private LocationTracker location;
-	
+
 	private ScreenWaker screenWaker;
-	
+
 	private FilteredOrientationTracker tracker;
-	
+
 	private Detector swipes;
-	
+
 	private String email;
-	
+
+	private HeadMotion lastHeadMotion;
+
 	private boolean resumed;
-	
+
 	private boolean setup;
-	
+
 	@InjectView(R.id.understandCount)
 	private TextView understandCountView;
-	
+
 	@InjectView(R.id.dontUnderstandCount)
 	private TextView dontUnderstandCountView;
 
 	@InjectView(R.id.understandCountTotal)
 	private TextView understandCountTotalView;
-	
+
 	@InjectView(R.id.dontUnderstandCountTotal)
 	private TextView dontUnderstandCountTotalView;
-	
+
 	@InjectView(R.id.userCount)
 	private TextView userCountView;
-	
+
 	@InjectView(R.id.titleBar)
 	private View titleBar;
-	
+
 	@InjectView(R.id.classTitle)
 	private TextView classTitle;
-	
+
 	private int understandCount;
-	
+
 	private int dontUnderstandCount;
-	
+
 	@InjectView(R.id.glassInstructions)
 	private View glassInstructions;
-	
+
 	@InjectView(R.id.androidButtons)
 	private View androidButtons;
 
@@ -96,37 +102,38 @@ public class MainActivity extends RoboActivity {
 
 	@InjectView(R.id.dontUnderstandButton)
 	private View dontUnderstandButton;
-	
+
 	private String className = "ADV 320F";
-		
+
 	private ClassStatus classStatus;
-	
+
 	private Runnable pollServer = new Runnable() {
 		@Override
 		public void run() {
 			Log.i(LOG_TAG, "pollServer#run");
-			
+
 			// Poll the server if we aren't currently sending something
 			if (!client.isSending()) {
 				Log.i(LOG_TAG, "polling server...");
 				client.sendToServer(understandCount, dontUnderstandCount, location, email, className);
 			} else {
-				Log.i(LOG_TAG, "skipping poll...");				
+				Log.i(LOG_TAG, "skipping poll...");
 			}
-			
+
 			// Run again later
-			if ( null != handler ) {
+			if (null != handler) {
 				handler.postDelayed(this, POLL_PERIOD_MS);
 			}
-		}		
+		}
 	};
-	
+
 	private DetectorListener detectorListener = new DetectorListener() {
 		@Override
 		public void onSwipeDownOrBack() {
 			Log.i(LOG_TAG, "onSwipeDownOrBack");
 			finish();
 		}
+
 		@Override
 		public void onSwipeForwardOrVolumeUp() {
 			Log.i(LOG_TAG, "onSwipeForwardOrVolumeUp");
@@ -138,91 +145,141 @@ public class MainActivity extends RoboActivity {
 			Log.i(LOG_TAG, "onSwipeBackOrVolumeDown");
 			onDontUnderstand();
 		}
+
 		@Override
 		public void onTap() {
 			Log.i(LOG_TAG, "onTap");
 		}
 	};
-	
+
 	private ApiClientListener clientListener = new ApiClientListener() {
 
 		@Override
 		public void onSendSuccess(ClassStatus result) {
 			Log.i(LOG_TAG, "onSendSuccess result = " + result);
 			MainActivity.this.classStatus = result;
-			updateUi();	
+			updateUi();
 		}
 
 		@Override
 		public void onSendFail() {
 			Log.i(LOG_TAG, "onSendFail");
-		}		
-	};
-	
-	private ApiClient client = new ApiClient(clientListener);
-		
-	private FilteredOrientationTracker.Listener trackerListener = new FilteredOrientationTracker.Listener() {		
-		@Override
-		public void onUpdate(float[] gyro, float[] gyroSum) {
-			//Log.i(LOG_TAG, "xGyro = " + gyro[1] + " xGyroSum = " 
-			//		+ gyroSum[1] + " yGyro = " + gyro[0] + " yGyroSum = " + gyroSum[0]);
-			// Nod head up and down
-			if ( Math.abs(gyroSum[0]) > NOD_TRIGGER_SUM) {
-				gyroSum[0] = 0;
-				onUnderstand();
-			}
-			// Shake head left and right 			
-			if ( Math.abs(gyroSum[1]) > SHAKE_TRIGGER_SUM) {
-				gyroSum[1] = 0;
-				onDontUnderstand();
-			}
 		}
 	};
-	
+
+	private ApiClient client = new ApiClient(clientListener);
+
+	private FilteredOrientationTracker.Listener trackerListener = new FilteredOrientationTracker.Listener() {
+		@Override
+		public void onUpdate(float[] gyro, float[] gyroSum) {
+			Log.i(LOG_TAG, "xGyro = " + gyro[1] + " xGyroSum = "
+					+ gyroSum[1] + " yGyro = " + gyro[0] + " yGyroSum = " +
+					gyroSum[0]);
+
+			// Check if we have a new head motion
+			HeadMotion newHeadMotion = null;
+			// Nod head up and down
+			if (Math.abs(gyroSum[0]) > NOD_TRIGGER_SUM && gyroSum[0] < 0) {
+				Log.i(LOG_TAG, "head up");
+				newHeadMotion = HeadMotion.UP;
+			} else if (Math.abs(gyroSum[0]) > NOD_TRIGGER_SUM && gyroSum[0] > 0) {
+				Log.i(LOG_TAG, "head down");
+				newHeadMotion = HeadMotion.DOWN;
+
+				// Shake head left and right
+			} else if (Math.abs(gyroSum[1]) > SHAKE_TRIGGER_SUM && gyroSum[1] < 0) {
+				Log.i(LOG_TAG, "head left");
+				newHeadMotion = HeadMotion.LEFT;
+			} else if (Math.abs(gyroSum[1]) > SHAKE_TRIGGER_SUM && gyroSum[1] > 0) {
+				Log.i(LOG_TAG, "head right");
+				newHeadMotion = HeadMotion.RIGHT;
+			}
+
+			// If we did, clear the movement sum
+			if (newHeadMotion == null) {
+				return;
+			}
+			gyroSum[0] = 0;
+			gyroSum[1] = 0;
+
+			Log.i(LOG_TAG, "last = " + lastHeadMotion + ", new = " + newHeadMotion);
+
+			// If we just did the opposite head motion, trigger a nod or shake
+			if (HeadMotion.LEFT == lastHeadMotion &&
+					HeadMotion.RIGHT == newHeadMotion) {
+				Log.i(LOG_TAG, "left right");
+				lastHeadMotion = null;
+				onDontUnderstand();
+				return;
+			} else if (HeadMotion.RIGHT == lastHeadMotion &&
+					HeadMotion.LEFT == newHeadMotion) {
+				Log.i(LOG_TAG, "right left");
+				lastHeadMotion = null;
+				onDontUnderstand();
+				return;
+			} else if (HeadMotion.UP == lastHeadMotion &&
+					HeadMotion.DOWN == newHeadMotion) {
+				Log.i(LOG_TAG, "up down");
+				lastHeadMotion = null;
+				onUnderstand();
+				return;
+			} else if (HeadMotion.DOWN == lastHeadMotion &&
+					HeadMotion.UP == newHeadMotion) {
+				Log.i(LOG_TAG, "down left");
+				lastHeadMotion = null;
+				onUnderstand();
+				return;
+			}
+
+			Log.i(LOG_TAG, "saving motion to check against next");
+			lastHeadMotion = newHeadMotion;
+		}
+	};
+
 	private void onUnderstand() {
 		Log.i(LOG_TAG, "onUnderstand");
-		
+
 		understandCount++;
 		client.sendToServer(understandCount, dontUnderstandCount, location, email, className);
-		updateUi();		
+		updateUi();
 	}
-	
+
 	private void onDontUnderstand() {
 		Log.i(LOG_TAG, "onDontUnderstand");
-		
+
 		dontUnderstandCount++;
 		client.sendToServer(understandCount, dontUnderstandCount, location, email, className);
-		updateUi();		
+		updateUi();
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.i(LOG_TAG, "onCreate");
 		screenWaker = new ScreenWaker(this);
-		
+
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 		super.onCreate(savedInstanceState);
-		
+
 		// Parse response to voice prompt, if any
 		final Intent intent = getIntent();
-		if ( null != intent ) {
+		if (null != intent) {
 			final Bundle extras = intent.getExtras();
-			if ( null != extras ) {
+			if (null != extras) {
 				if (extras.containsKey(RecognizerIntent.EXTRA_RESULTS)) {
-					final ArrayList<String> voiceResults = 
+					final ArrayList<String> voiceResults =
 							extras.getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
 					if (!voiceResults.isEmpty()) {
 						className = voiceResults.get(0);
 					}
-				}				
-			}			
+				}
+			}
 		}
-		
-		setContentView(R.layout.activity_main);	
+
+		setContentView(R.layout.activity_main);
 		classTitle.setText(className);
-		
+
 		understandButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -235,34 +292,34 @@ public class MainActivity extends RoboActivity {
 				onDontUnderstand();
 			}
 		});
-		
+
 		email = DeviceEmail.get(this);
 		Log.i(LOG_TAG, "user email = " + email);
-				
+
 		updateUi();
-		
+
 		if (Build.MODEL.toUpperCase().contains("GLASS")) {
-				Log.d(LOG_TAG, "Glass detected, tracking side touch pad events...");
+			Log.d(LOG_TAG, "Glass detected, tracking side touch pad events...");
 			swipes = GlassSetup.setup(detectorListener, this);
 			androidButtons.setVisibility(View.GONE);
 			titleBar.setVisibility(View.GONE);
 		} else {
-				Log.d(LOG_TAG, "Not Glass: " + Build.MODEL);
-				glassInstructions.setVisibility(View.GONE);
+			Log.d(LOG_TAG, "Not Glass: " + Build.MODEL);
+			glassInstructions.setVisibility(View.GONE);
 		}
 	}
-		
+
 	@Override
-	public boolean onGenericMotionEvent(MotionEvent event) {	
+	public boolean onGenericMotionEvent(MotionEvent event) {
 		Log.i(LOG_TAG, "onGenericMotionEvent");
-		
+
 		swipes.onGenericMotionEvent(event);
 		return super.onGenericMotionEvent(event);
 	}
 
 	private void updateUi() {
 		Log.i(LOG_TAG, "updateUi");
-		
+
 		understandCountView.setText("Understand: " + understandCount);
 		dontUnderstandCountView.setText("Don't understand: " + dontUnderstandCount);
 		if (null != classStatus) {
@@ -275,7 +332,7 @@ public class MainActivity extends RoboActivity {
 	@Override
 	protected void onResume() {
 		Log.i(LOG_TAG, "onResume hasFocus = " + hasWindowFocus());
-		
+
 		super.onResume();
 		resumed = true;
 		setupOrCleanup();
@@ -285,7 +342,7 @@ public class MainActivity extends RoboActivity {
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
 		Log.i(LOG_TAG, "onWindowFocusChanged hasFocus = " + hasFocus);
-		
+
 		super.onWindowFocusChanged(hasFocus);
 		setupOrCleanup();
 	}
@@ -293,61 +350,62 @@ public class MainActivity extends RoboActivity {
 	@Override
 	protected void onPause() {
 		Log.i(LOG_TAG, "onPause hasFocus = " + hasWindowFocus());
-		
-		super.onPause();		
+
+		super.onPause();
 		resumed = false;
 		setupOrCleanup();
 		screenWaker.onPause();
 	}
-	
+
 	/**
-	 * Glass has a nasty habit of resuming, pausing, resuming, then giving window focus.
-	 * So delay setup until we are both resumed and have window focus, otherwise cleanup.
+	 * Glass has a nasty habit of resuming, pausing, resuming, then giving
+	 * window focus. So delay setup until we are both resumed and have window
+	 * focus, otherwise cleanup.
 	 **/
 	private void setupOrCleanup() {
 		Log.i(LOG_TAG, "setupOrCleanup");
-		
+
 		// Cleanup
-		if ( !resumed && !hasWindowFocus() && setup ) {
+		if (!resumed && !hasWindowFocus() && setup) {
 			Log.i(LOG_TAG, "cleaning up...");
 			setup = false;
-			if ( null != location ) {
+			if (null != location) {
 				location.stopListeningForLocations();
 				location = null;
 			}
-			
-			if ( null != tracker ) {
+
+			if (null != tracker) {
 				tracker.onPause();
 				tracker = null;
 			}
-			
-			if ( null != handler ) {
+
+			if (null != handler) {
 				handler.removeCallbacksAndMessages(null);
 				handler = null;
 			}
-			
+
 			return;
 		}
-		
+
 		// Startup
 		if (resumed && hasWindowFocus() && !setup) {
 			Log.i(LOG_TAG, "setup...");
 			setup = true;
-			if ( null == location ) {
+			if (null == location) {
 				location = new LocationTracker(this);
 				location.startAccquiringLocationData();
 			}
-			
-			if ( null == tracker ) {
+
+			if (null == tracker) {
 				tracker = new FilteredOrientationTracker(this, trackerListener);
 				tracker.onResume();
 			}
-			
-			if ( null == handler ) {
+
+			if (null == handler) {
 				handler = new Handler();
 				handler.postDelayed(pollServer, POLL_PERIOD_MS);
 			}
 		}
 	}
-		
+
 }
