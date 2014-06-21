183a68c0622439c0e5c1949c25dc957a84bd305f
Lance Nanek <lnanek@gmail.com>
2014-06-20 18:54:40 -0700

    Always track latest location to upload to server later

---
+++ android-client/src/us/gpop/classpulse/LocationTracker.java
@@ -0,0 +1,84 @@
+package us.gpop.classpulse;
+
+import java.util.List;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Tracks the most accurate location yet found.
+ */
+public class LocationTracker {
+	public static final String TAG = LocationTracker.class.getSimpleName();
+	final LocationManager locationManager;
+	private Float locationAccuracy;
+	private Double longitude;
+	private Double latitude;
+
+	private LocationListener locationListener = new LocationListener() {
+		@Override
+		public void onLocationChanged(Location location) {
+			if (null == location) {
+				return;
+			}
+			// Take any location if we have none, otherwise take most accurate
+			// vs. past
+			if (null == locationAccuracy || locationAccuracy > location.getAccuracy()) {
+				latitude = location.getLatitude();
+				longitude = location.getLongitude();
+				locationAccuracy = location.getAccuracy();
+			}
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+
+		@Override
+		public void onStatusChanged(String provider,
+				int status, Bundle extras) {
+		}
+	};
+	
+	public LocationTracker(final Context context) {
+		locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+	}
+
+	public void startAccquiringLocationData() {
+		Log.d(TAG, "accquireLocationData");
+
+		final List<String> providers = locationManager.getAllProviders();
+		for (final String provider : providers) {
+			final Location lastKnownLocation = locationManager.getLastKnownLocation(provider);
+			locationListener.onLocationChanged(lastKnownLocation);
+		}
+		for (final String provider : providers) {
+			locationManager.requestLocationUpdates(provider, 0, 0, locationListener);
+		}
+	}
+	
+	public void stopListeningForLocations() {
+		Log.d(TAG, "stopListeningForLocations");		
+		locationManager.removeUpdates(locationListener);
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+	
+	public boolean hasLocation() {
+		return null != getLatitude() && null != getLongitude();
+	}
+}
--- android-client/src/us/gpop/classpulse/MainActivity.java
+++ android-client/src/us/gpop/classpulse/MainActivity.java
@@ -2,12 +2,80 @@ package us.gpop.classpulse;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+	
+	private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+	private LocationTracker location;
+	
+	private boolean resumed;
+	
+	private boolean setup;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Log.i(LOG_TAG, "onCreate");
+		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);		
 	}
+
+	@Override
+	protected void onResume() {
+		Log.i(LOG_TAG, "onResume hasFocus = " + hasWindowFocus());
+		
+		super.onResume();
+		resumed = true;
+		setupOrCleanup();
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		Log.i(LOG_TAG, "onWindowFocusChanged hasFocus = " + hasFocus);
+		
+		super.onWindowFocusChanged(hasFocus);
+		setupOrCleanup();
+	}
+
+	@Override
+	protected void onPause() {
+		Log.i(LOG_TAG, "onPause hasFocus = " + hasWindowFocus());
+		
+		super.onPause();		
+		resumed = false;
+		setupOrCleanup();
+	}
+	
+	/**
+	 * Glass has a nasty habit of resuming, pausing, resuming, then giving window focus.
+	 * So delay setup until we are both resumed and have window focus, otherwise cleanup.
+	 **/
+	private void setupOrCleanup() {
+		Log.i(LOG_TAG, "setupOrCleanup");
+		
+		// Cleanup
+		if ( !resumed && !hasWindowFocus() && setup ) {
+			Log.i(LOG_TAG, "cleaning up...");
+			setup = false;
+			if ( null != location ) {
+				location.stopListeningForLocations();
+				location = null;
+			}
+			return;
+		}
+		
+		// Startup
+		if (resumed && hasWindowFocus() && !setup) {
+			Log.i(LOG_TAG, "setup...");
+			setup = true;
+			if ( null == location ) {
+				location = new LocationTracker(this);
+				location.startAccquiringLocationData();
+			}
+		}
+	}
+	
+	
 }
